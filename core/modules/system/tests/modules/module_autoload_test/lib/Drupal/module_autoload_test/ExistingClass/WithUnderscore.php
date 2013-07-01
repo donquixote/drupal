510a82b264200d306c96f3931ff6fceddec045f4
Andreas Hennings <andreas@dqxtech.net>
2013-07-01 02:07:57 +0200

    Test edge cases of PSR-0 class loading.

---
+++ core/modules/system/tests/modules/module_autoload_test/lib/Drupal/module_autoload_test/ExistingClass/WithUnderscore.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\module_autoload_test\ExistingClass_WithUnderscore.
+ */
+
+namespace Drupal\module_autoload_test;
+
+class ExistingClass_WithUnderscore {}
---
+++ core/modules/system/tests/modules/module_autoload_test/lib/Drupal/module_autoload_test_subnamespace/PrefixTestClass.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\module_autoload_test_subnamespace\PrefixTestClass.
+ */
+
+namespace Drupal\module_autoload_test_subnamespace;
+
+class PrefixTestClass {}
