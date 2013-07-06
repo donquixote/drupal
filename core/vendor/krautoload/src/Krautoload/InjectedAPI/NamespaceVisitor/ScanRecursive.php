e75db55d6233cbe07b557a883981952fa4aa7d7f
Andreas Hennings <andreas@dqxtech.net>
2013-07-06 10:20:29 +0200

    remove some debug code

--- core/vendor/krautoload/src/Krautoload/InjectedAPI/NamespaceVisitor/ScanRecursive.php
+++ core/vendor/krautoload/src/Krautoload/InjectedAPI/NamespaceVisitor/ScanRecursive.php
@@ -5,12 +5,6 @@ namespace Krautoload;
 class InjectedAPI_NamespaceVisitor_ScanRecursive extends InjectedAPI_NamespaceVisitor_ScanAbstract {
 
   public function namespaceDirectoryPlugin($baseDir, $relativePath, $plugin) {
-    if (is_array($baseDir)) {
-      throw new \Exception("Base dir must not be array.");
-    }
-    if (is_array($relativePath)) {
-      throw new \Exception("Relative path must not be array.");
-    }
     $plugin->pluginScanRecursive($this->api, $baseDir, $relativePath);
   }
 }
