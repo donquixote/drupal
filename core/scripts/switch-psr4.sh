53e69672f55af63a22407b6b9916592224cefaef
Andreas Hennings <andreas@dqxtech.net>
2013-07-19 03:14:10 +0200

    Extend the PSR-4 conversion script to also move classes in tests folders.

--- core/scripts/switch-psr4.sh
+++ core/scripts/switch-psr4.sh
@@ -15,10 +15,10 @@ while (!defined('DRUPAL_ROOT')) {
   $dir = dirname($dir);
 }
 
-process_modules_dir(DRUPAL_ROOT . '/core/modules');
-process_modules_dir(DRUPAL_ROOT . '/core/profiles');
+process_extensions_base_dir(DRUPAL_ROOT . '/core/modules');
+process_extensions_base_dir(DRUPAL_ROOT . '/core/profiles');
 
-function process_modules_dir($dir) {
+function process_extensions_base_dir($dir) {
   /**
    * @var SplFileInfo $fileinfo
    */
@@ -27,62 +27,77 @@ function process_modules_dir($dir) {
       // do nothing
     }
     elseif ($fileinfo->isDir()) {
-      process_module_dir($fileinfo->getPathname());
+      process_candidate_dir($fileinfo->getPathname());
     }
   }
 }
 
-function process_module_dir($dir) {
-  $has = array(
-    'lib' => FALSE,
-    'src' => FALSE,
-    'info' => FALSE,
-  );
+function process_candidate_dir($dir) {
   foreach (new \DirectoryIterator($dir) as $fileinfo) {
     if ($fileinfo->isDot()) {
-      // do nothing.
+      // Ignore "." and "..".
     }
     elseif ($fileinfo->isDir()) {
-      if ('lib' === $fileinfo->getFilename()) {
-        $has['lib'] = TRUE;
-      }
-      elseif ('src' === $fileinfo->getFilename()) {
-        $has['src'] = TRUE;
-      }
-      elseif ('module_autoload_test' === $fileinfo->getFilename()) {
-        // Do not move anything in this testing module,
-        // which already has an src folder.
-      }
-      else {
-        process_module_dir($fileinfo->getPathname());
+      // It's a directory.
+      switch ($fileinfo->getFilename()) {
+        case 'lib':
+        case 'src':
+        case 'module_autoload_test':
+          // Ignore these directory names.
+          continue;
+        default:
+          // Look for more extensions in subdirectories.
+          process_candidate_dir($fileinfo->getPathname());
       }
     }
     else {
+      // It's a file.
       if (preg_match('/^(.+).info.yml$/', $fileinfo->getFilename(), $m)) {
+        // It's a *.info.yml file, so we found an extension directory.
         $extension_name = $m[1];
-        $has['info'] = TRUE;
       }
     }
   }
-  if ($has['lib'] && $has['info']) {
-    if (is_dir("$dir/lib/Drupal/$extension_name")) {
-      // This is a module directory with a /lib/ folder.
-      if (!$has['src']) {
-        mkdir($dir . '/src');
-      }
-      rename("$dir/lib/Drupal/$extension_name", "$dir/src");
+  if (isset($extension_name)) {
+    process_extension($extension_name, $dir);
+  }
+}
+
+function process_extension($name, $dir) {
+
+  // Move main module class files.
+  if (is_dir("$dir/lib/Drupal/$name")) {
+    // This is a module directory with a PSR-0 /lib/ folder.
+    if (!is_dir($dir . '/src')) {
+      mkdir($dir . '/src');
     }
-    if (is_dir_empty("$dir/lib/Drupal")) {
-      rmdir("$dir/lib/Drupal");
-    }
-    if (is_dir_empty("$dir/lib")) {
-      rmdir("$dir/lib");
+    rename("$dir/lib/Drupal/$name", "$dir/src");
+  }
+
+  // Move class files in tests directory.
+  if (is_dir("$dir/tests/Drupal/$name/Tests")) {
+    rename("$dir/tests/Drupal/$name/Tests", "$dir/tests/src");
+  }
+
+  // Clean up empty directories.
+  foreach (array(
+    "lib/Drupal/$name",
+    'lib/Drupal',
+    'lib',
+    "tests/Drupal/$name/Tests",
+    "tests/Drupal/$name",
+    "tests/Drupal",
+  ) as $subdir) {
+    if (is_dir_empty("$dir/$subdir")) {
+      rmdir("$dir/$subdir");
     }
   }
 }
 
 function is_dir_empty($dir) {
-  if (!is_readable($dir)) return NULL;
+  if (!is_readable($dir)) {
+    return NULL;
+  }
   $handle = opendir($dir);
   while (false !== ($entry = readdir($handle))) {
     if ($entry != "." && $entry != "..") {
