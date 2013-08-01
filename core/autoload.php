be3119cc6710571d3e48cace8167b52ec61f9da8
Andreas Hennings <andreas@dqxtech.net>
2013-08-01 04:11:25 +0200

    let custom autoload.php use drupal's custom loader instead of composer's.

--- core/autoload.php
+++ core/autoload.php
@@ -1,3 +1,12 @@
 <?php
 
-return __DIR__ . '/vendor/autoload.php';
+use Drupal\Component\Autoload\ClassLoaderInterface;
+use Drupal\Component\Autoload\ClassLoader;
+
+$drupal_root = dirname(__DIR__);
+
+// Include the Drupal ClassLoader for loading PSR-0-compatible classes.
+require_once $drupal_root . '/core/lib/Drupal/Core/ClassLoader/ClassLoaderInterface.php';
+require_once $drupal_root . '/core/lib/Drupal/Core/ClassLoader/AbstractClassLoader.php';
+require_once $drupal_root . '/core/lib/Drupal/Core/ClassLoader/ClassLoader.php';
+$loader = new ClassLoader();
