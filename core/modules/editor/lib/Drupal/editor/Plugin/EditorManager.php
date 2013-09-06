8a51b2a0e565adfd664545df9419cb1f460ba173
Andreas Hennings <andreas@dqxtech.net>
2013-09-06 02:31:35 +0200

    change constructor signature of DefaultPluginManager, and give it an addAnnotationClass() method.

--- core/modules/editor/lib/Drupal/editor/Plugin/EditorManager.php
+++ core/modules/editor/lib/Drupal/editor/Plugin/EditorManager.php
@@ -31,8 +31,8 @@ class EditorManager extends DefaultPluginManager {
    *   The module handler to invoke the alter hook with.
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
-    $annotation_namespaces = array('Drupal\editor\Annotation' => $namespaces['Drupal\editor'] . '/Annotation');
-    parent::__construct('Plugin/Editor', $namespaces, $annotation_namespaces, 'Drupal\editor\Annotation\Editor');
+    parent::__construct($namespaces, 'Plugin\Editor', 'Drupal\editor\Annotation\Editor');
+    $this->addAnnotationNamespace('Drupal\editor\Annotation');
     $this->alterInfo($module_handler, 'editor_info');
     $this->setCacheBackend($cache_backend, $language_manager, 'editor');
   }
