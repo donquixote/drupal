c0ea89be16f388ddf947691fb47af837d8ba17e0
Andreas Hennings <andreas@dqxtech.net>
2013-09-20 16:03:58 +0200

    autoload: Create a Drupal-specific ClassLoader, and use it instead of the one provided by Composer. For the start, this is simply a copy of Composer's ClassLoader. Use a post-autoload-dump script registered in composer.json, to replace the core/vendor/autoload.php with a Drupal-specific one.

---
+++ core/lib/Drupal/Core/Autoload/GeneratorScript.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Plugin\Discovery\GeneratorScripts.
+ */
+
+namespace Drupal\Core\Autoload;
+
+use Composer\Script\Event;
+
+/**
+ * Script to be called after composer generated the autoload files.
+ */
+class GeneratorScript {
+
+  /**
+   * Called after composer autoload-dump.
+   * Replaces the core/vendor/autoload.php with a Drupal-specific autoload.php.
+   *
+   * @param \Composer\Script\Event $event
+   */
+  public static function postAutoloadDump(Event $event) {
+    $core_dir = dirname(dirname(dirname(dirname(__DIR__))));
+    // Replace the generated core/vendor/autoload.php.
+    copy($core_dir . '/autoload.drupal.php', $core_dir . '/vendor/autoload.php');
+  }
+} 
