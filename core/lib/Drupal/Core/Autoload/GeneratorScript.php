ac79a24cf6d8e1fcb53b2f45697f683417995fa0
Andreas Hennings <andreas@dqxtech.net>
2013-11-18 22:47:10 +0100

    Docblock fixes, and fix undefined  in DrupalAutoloaderInit.

--- core/lib/Drupal/Core/Autoload/GeneratorScript.php
+++ core/lib/Drupal/Core/Autoload/GeneratorScript.php
@@ -3,26 +3,35 @@
 /**
  * @file
  * Contains \Drupal\Core\Plugin\Discovery\GeneratorScripts.
+ *
+ * This file is only ever included from Composer commands. This is why it can
+ * use the class Composer\Script\Event, even though this class is not part of
+ * Drupal core, or any vendor libraries shipped with Drupal.
  */
 
-namespace Drupal\Core\Autoload;
+namespace Drupal\Core\Autoload {
 
-use Composer\Script\Event;
-
-/**
- * Script to be called after composer generated the autoload files.
- */
-class GeneratorScript {
+use /** @noinspection PhpUndefinedClassInspection */
+  Composer\Script\Event;
 
   /**
-   * Called after composer autoload-dump.
-   * Replaces the core/vendor/autoload.php with a Drupal-specific autoload.php.
-   *
-   * @param \Composer\Script\Event $event
+   * Script to be called after Composer generated the autoload files.
    */
-  public static function postAutoloadDump(Event $event) {
-    $core_dir = dirname(dirname(dirname(dirname(__DIR__))));
-    // Replace the generated core/vendor/autoload.php.
-    copy($core_dir . '/autoload.drupal.php', $core_dir . '/vendor/autoload.php');
+  class GeneratorScript {
+
+    /**
+     * Replaces the core/vendor/autoload.php with a Drupal-specific autoload.php.
+     *
+     * Registered as a post-autoload-dump callback in composer.json, to be run
+     * every time after Composer has generated the autoload files.
+     *
+     * @param \Composer\Script\Event $event
+     *   Event object provided by Composer.
+     */
+    public static function postAutoloadDump(Event $event) {
+      $core_dir = dirname(dirname(dirname(dirname(__DIR__))));
+      // Replace the generated core/vendor/autoload.php.
+      copy($core_dir . '/autoload.drupal.php', $core_dir . '/vendor/autoload.php');
+    }
   }
-} 
+}
