289eb13ecbfde4e31e1b1fbaeffa6d4a75af05e2
Andreas Hennings <andreas@dqxtech.net>
2014-01-07 23:27:14 +0100

    Add interface methods in StatementInterface for PDO methods setFetchMode(), fetch(), fetchObject(), fetchAll(), in a way that does not conflict with PDO.

--- core/modules/migrate/tests/Drupal/migrate/Tests/FakeStatement.php
+++ core/modules/migrate/tests/Drupal/migrate/Tests/FakeStatement.php
@@ -101,4 +101,82 @@ class FakeStatement extends \ArrayIterator implements StatementInterface {
     );
   }
 
+  /**
+   * Sets the default fetch mode for this statement.
+   *
+   * See http://php.net/manual/pdo.constants.php for the definition of the
+   * constants used.
+   *
+   * @param $mode
+   *   One of the PDO::FETCH_* constants.
+   * @param $a1
+   *   An option depending of the fetch mode specified by $mode:
+   *   - for PDO::FETCH_COLUMN, the index of the column to fetch
+   *   - for PDO::FETCH_CLASS, the name of the class to create
+   *   - for PDO::FETCH_INTO, the object to add the data to
+   * @param $a2
+   *   If $mode is PDO::FETCH_CLASS, the optional arguments to pass to the
+   *   constructor.
+   */
+  public function setFetchMode($mode, $a1 = NULL, $a2 = array()) {
+    // TODO: Implement setFetchMode() method.
+  }
+
+  /**
+   * Fetches the next row from a result set.
+   *
+   * See http://php.net/manual/pdo.constants.php for the definition of the
+   * constants used.
+   *
+   * @param int|NULL $mode
+   *   One of the PDO::FETCH_* constants.
+   *   Default to what was specified by setFetchMode().
+   * @param $cursor_orientation
+   *   Not implemented in all database drivers, don't use.
+   * @param $cursor_offset
+   *   Not implemented in all database drivers, don't use.
+   *
+   * @return mixed
+   *   A result, formatted according to $mode.
+   */
+  public function fetch(
+    $mode = NULL,
+    $cursor_orientation = NULL,
+    $cursor_offset = NULL
+  ) {
+    // TODO: Implement fetch() method.
+  }
+
+  /**
+   * Fetches the next row and returns it as an object.
+   *
+   * The object will be of the class specified by StatementInterface::setFetchMode()
+   * or stdClass if not specified.
+   *
+   * @return object|\stdClass|false
+   */
+  public function fetchObject() {
+    // TODO: Implement fetchObject() method.
+  }
+
+  /**
+   * Returns an array containing all of the result set rows.
+   *
+   * @param int|NULL $mode
+   *   One of the PDO::FETCH_* constants.
+   * @param int|NULL $column_index
+   *   If $mode is PDO::FETCH_COLUMN, the index of the column to fetch.
+   * @param array|NULL $constructor_arguments
+   *   If $mode is PDO::FETCH_CLASS, the arguments to pass to the constructor.
+   *
+   * @return array
+   *   An array of results.
+   */
+  function fetchAll(
+    $mode = NULL,
+    $column_index = NULL,
+    array $constructor_arguments = NULL
+  ) {
+    // TODO: Implement fetchAll() method.
+  }
 }
