f5f14df0679e6187d672321bef63149582fe8622
Andreas Hennings <andreas@dqxtech.net>
2014-03-02 15:34:48 +0100

    Factor out SiteDirectory and SitePicker from Site.

--- core/lib/Drupal/Core/Site/Site.php
+++ core/lib/Drupal/Core/Site/Site.php
@@ -20,13 +20,9 @@ class Site {
   private $root;
 
   /**
-   * The relative path to the site directory.
-   *
-   * May be an empty string, in case the site directory is the root directory.
-   *
-   * @var string
+   * @var SiteDirectory|null
    */
-  private $path;
+  private $siteDirectory;
 
   /**
    * Whether the Site singleton was instantiated by the installer.
@@ -63,6 +59,7 @@ class Site {
    *   (optional) An explicit site path to set; skipping site negotiation.
    *   This can be defined as $conf_path in the root /settings.php file.
    *
+   * @throws \BadMethodCallException
    * @see drupal_settings_initialize()
    */
   public static function init($root_directory, array $sites = NULL, $custom_path = NULL) {
@@ -70,7 +67,7 @@ class Site {
       // Only the installer environment is allowed to instantiate the Site
       // singleton prior to drupal_settings_initialize().
       // @see Site::initInstaller()
-      if (!self::$instance->isInstaller()) {
+      if (!self::$instance->isInstaller) {
         throw new \BadMethodCallException('Site path is initialized already.');
       }
     }
@@ -80,7 +77,7 @@ class Site {
     self::$instance->initializePath($sites, $custom_path);
 
     // Prevent this method from being called more than once.
-    if (self::$instance->isInstaller()) {
+    if (self::$instance->isInstaller) {
       self::$instance->isInstaller = FALSE;
     }
   }
@@ -96,6 +93,7 @@ class Site {
    * @param string $root_directory
    *   The root directory to use for absolute paths; i.e., DRUPAL_ROOT.
    *
+   * @throws \BadMethodCallException
    * @see install_begin_request()
    */
   public static function initInstaller($root_directory) {
@@ -109,6 +107,10 @@ class Site {
 
   /**
    * Constructs the Site singleton.
+   *
+   * @param string $root_directory
+   *
+   * @throws \BadMethodCallException
    */
   private function __construct($root_directory) {
     if (isset(self::$instance)) {
@@ -154,103 +156,36 @@ class Site {
   }
 
   /**
-   * Returns whether the Site singleton was instantiated for the installer.
-   */
-  private function isInstaller() {
-    return $this->isInstaller;
-  }
-
-  /**
    * Initializes the site path.
    *
-   * @param array $sites
-   *   (optional) A multi-site mapping, as defined in settings.php.
+   * @param array|null $sites
+   *   (optional) A multi-site mapping, as defined in settings.php,
+   *   or NULL if no multi-site functionality is enabled.
    * @param string $custom_path
    *   (optional) An explicit site path to set; skipping site negotiation.
    */
   private function initializePath(array $sites = NULL, $custom_path = NULL) {
     // Force-override the site directory in tests.
     if ($test_prefix = drupal_valid_test_ua()) {
-      $this->path = 'sites/simpletest/' . substr($test_prefix, 10);
+      $path = 'sites/simpletest/' . substr($test_prefix, 10);
     }
     // An explicitly defined $conf_path in /settings.php takes precedence.
     elseif (isset($custom_path)) {
-      $this->path = $custom_path;
+      $path = $custom_path;
     }
     // If the multi-site functionality was enabled in /settings.php, discover
     // the path for the current site.
     // $sites just needs to be defined; an explicit mapping is not required.
     elseif (isset($sites)) {
-      $this->path = $this->determinePath($sites, !$this->isInstaller());
+      $site_picker = new SitePicker($this->root, $sites);
+      $path = $site_picker->determinePath(!$this->isInstaller);
     }
     // If the multi-site functionality is not enabled, the Drupal root
     // directory is the site directory.
     else {
-      $this->path = '';
+      $path = '';
     }
-  }
-
-  /**
-   * Finds the appropriate configuration directory for a given host and path.
-   *
-   * Finds a matching configuration directory file by stripping the website's
-   * hostname from left to right and pathname from right to left. By default,
-   * the directory must contain a 'settings.php' file for it to match. If the
-   * parameter $require_settings is set to FALSE, then a directory without a
-   * 'settings.php' file will match as well. The first configuration
-   * file found will be used and the remaining ones will be ignored.
-   *
-   * The settings.php file can define aliases in an associative array named
-   * $sites. For example, to create a directory alias for
-   * http://www.drupal.org:8080/mysite/test whose configuration file is in
-   * sites/example.com, the array should be defined as:
-   * @code
-   * $sites = array(
-   *   '8080.www.drupal.org.mysite.test' => 'example.com',
-   * );
-   * @endcode
-   *
-   * @see default.settings.php
-   *
-   * @param array $sites
-   *   A multi-site mapping, as defined in settings.php.
-   * @param bool $require_settings
-   *   Only configuration directories with an existing settings.php file
-   *   will be recognized. Defaults to TRUE. During initial installation,
-   *   this is set to FALSE so that Drupal can detect a matching directory,
-   *   then create a new settings.php file in it.
-   *
-   * @return string
-   *   The path of the matching configuration directory. May be an empty string,
-   *   in case the site configuration directory is the root directory.
-   */
-  private function determinePath(array $sites, $require_settings) {
-    // The hostname and optional port number, e.g. "www.example.com" or
-    // "www.example.com:8080".
-    $http_host = $_SERVER['HTTP_HOST'];
-    // The part of the URL following the hostname, including the leading slash.
-    $script_name = $_SERVER['SCRIPT_NAME'] ?: $_SERVER['SCRIPT_FILENAME'];
-
-    $uri = explode('/', $script_name);
-    $server = explode('.', implode('.', array_reverse(explode(':', rtrim($http_host, '.')))));
-    for ($i = count($uri) - 1; $i > 0; $i--) {
-      for ($j = count($server); $j > 0; $j--) {
-        $dir = implode('.', array_slice($server, -$j)) . implode('.', array_slice($uri, 0, $i));
-        // Check for an alias in $sites from settings.php.
-        if (isset($sites[$dir])) {
-          $dir = $sites[$dir];
-        }
-        if ($require_settings) {
-          if (file_exists($this->root . '/sites/' . $dir . '/settings.php')) {
-            return "sites/$dir";
-          }
-        }
-        elseif (file_exists($this->root . '/sites/' . $dir)) {
-          return "sites/$dir";
-        }
-      }
-    }
-    return '';
+    $this->siteDirectory = new SiteDirectory($path);
   }
 
   /**
@@ -273,28 +208,17 @@ class Site {
    * @param string $filepath
    *   The filepath to prefix.
    *
+   * @throws \RuntimeException
    * @return string
    *   The prefixed filepath.
    */
   private function resolvePath($filepath) {
     // Extra safety protection in case a script somehow manages to bypass all
     // other protections.
-    if (!isset($this->path)) {
+    if (!isset($this->siteDirectory)) {
       throw new \RuntimeException('Site path is not initialized yet.');
     }
-    // A faulty call to Site::getPath() might include a leading slash (/), in
-    // which case the entire site path resolution of this function would be
-    // pointless, because the resulting path would still be absolute. Therefore,
-    // guarantee that even a bogus argument is resolved correctly.
-    $filepath = ltrim($filepath, '/');
-
-    if ($this->path !== '') {
-      if ($filepath !== '') {
-        return $this->path . '/' . $filepath;
-      }
-      return $this->path;
-    }
-    return $filepath;
+    return $this->siteDirectory->resolvePath($filepath);
   }
 
   /**
