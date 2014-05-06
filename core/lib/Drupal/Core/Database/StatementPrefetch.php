be94a65be56e0cfbb7132846c384b0df0d0d5a43
Andreas Hennings <andreas@dqxtech.net>
2014-05-06 16:39:58 +0200

    Controversial {@inheritdoc} in dbtng

--- core/lib/Drupal/Core/Database/StatementPrefetch.php
+++ core/lib/Drupal/Core/Database/StatementPrefetch.php
@@ -365,6 +365,9 @@ class StatementPrefetch implements \Iterator, StatementInterface {
     return $this->rowCount;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetch($fetch_style = NULL, $cursor_orientation = \PDO::FETCH_ORI_NEXT, $cursor_offset = NULL) {
     if (isset($this->currentRow)) {
       // Set the fetch parameter.
@@ -398,10 +401,16 @@ class StatementPrefetch implements \Iterator, StatementInterface {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchField($index = 0) {
     return $this->fetchColumn($index);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchObject($class_name = NULL, $constructor_args = array()) {
     if (isset($this->currentRow)) {
       if (!isset($class_name)) {
