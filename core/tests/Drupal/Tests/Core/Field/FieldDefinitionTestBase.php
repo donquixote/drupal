f005a680de5bd6ed014b476c2ad121c9aa0eb7a1
Andreas Hennings <andreas@dqxtech.net>
2014-05-23 09:58:11 +0200

    OPTIONAL: Add default implementation for getModuleInfoFilePath(), so that subclasses don't need to implement it.

--- core/tests/Drupal/Tests/Core/Field/FieldDefinitionTestBase.php
+++ core/tests/Drupal/Tests/Core/Field/FieldDefinitionTestBase.php
@@ -85,10 +85,99 @@ abstract class FieldDefinitionTestBase extends UnitTestCase {
    * This will be used to determine both the module name and the module
    * directory.
    *
+   * This defaults to the module that contains the test, but can be overridden.
+   *
    * @return string
    *   The path to the MODULE.info.yml file, e.g.
    *   DRUPAL_ROOT . "/core/modules/path/path.info.yml".
+   *
+   * @throws \Exception
    */
-  abstract protected function getModuleInfoFilePath();
+  protected function getModuleInfoFilePath() {
+
+    $class = get_class($this);
+    $module_info_file = $this->classExtractModuleInfoFilePath($class);
+    if ($module_info_file === FALSE) {
+      throw new \Exception("No module info file found for class '$class'.");
+    }
+    return $module_info_file;
+  }
+
+  /**
+   * Determines if a given class is part of a Drupal module, and determines the
+   * path to the *.info.yml file, if possible.
+   *
+   * @todo Put this in a globally accessible place.
+   *
+   * @param string $class
+   *   The fully-qualified class name, e.g.
+   *   "Drupal\\path\\Tests\\PathFieldDefinitionTest".
+   *
+   * @return string|false
+   *   The path to the module info file, or FALSE if it could not be determined.
+   */
+  private function classExtractModuleInfoFilePath($class) {
+
+    if (!preg_match("/Drupal\\\\(.+)\\\\Tests\\\\(.+)$/", $class, $m)) {
+      return FALSE;
+    }
+
+    list(, $module_name, $relative_class_name) = $m;
+
+    $relative_path = str_replace('\\', '/', $relative_class_name) . '.php';
+
+    $suffixes = array(
+      // PSR-4 location for PHPUnit tests.
+      '/tests/src/' . $relative_path,
+      // PSR-0 location for PHPUnit tests.
+      // @todo Remove when PSR-0 support has ended.
+      "/tests/Drupal/$module_name/Tests/" . $relative_path,
+    );
+
+    $class_file = (new \ReflectionClass($class))->getFileName();
+    if ($class_file === FALSE) {
+      return FALSE;
+    }
+
+    // Normalize the directory separator to '/'.
+    $class_file = str_replace(DIRECTORY_SEPARATOR, '/', $class_file);
+
+    foreach ($suffixes as $suffix) {
+      $dir = $this->stringRemoveSuffix($class_file, $suffix);
+      if (FALSE !== $dir) {
+        $module_info_file = $dir . '/' . $module_name . '.info.yml';
+        if (is_file($module_info_file)) {
+          return $module_info_file;
+        }
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Removes a suffix from a string, if possible.
+   *
+   * @todo Put this in a globally accessible place.
+   *
+   * @param string $string
+   *   A string to test.
+   * @param string $suffix
+   *   A string that could be a suffix of $string.
+   *
+   * @return bool|string
+   *   The $prefix, so that $prefix . $suffix === $string, or
+   *   FALSE, if $string does not end with $suffix.
+   */
+  private function stringRemoveSuffix($string, $suffix) {
+    if (FALSE !== $pos = strrpos($string, $suffix)) {
+      if ($pos + strlen($suffix) === strlen($string)) {
+        // $string ends with $suffix, so return the beginning of $string.
+        return substr($string, 0, $pos);
+      }
+    }
+    // $string does not end with $suffix.
+    return FALSE;
+  }
 
 }
