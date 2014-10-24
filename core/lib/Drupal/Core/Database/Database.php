689e04b11a76fd3393028bfbd4b30937cb388b38
Andreas Hennings <andreas@dqxtech.net>
2014-10-25 00:47:12 +0200

    Introduce DatabaseManager, and change Database to use it. Please merge upwards!

--- core/lib/Drupal/Core/Database/Database.php
+++ core/lib/Drupal/Core/Database/Database.php
@@ -7,12 +7,16 @@
 
 namespace Drupal\Core\Database;
 
+use Drupal\Core\Database\Manager\DatabaseManager;
+
 /**
  * Primary front-controller for the database system.
  *
  * This class is uninstantiatable and un-extendable. It acts to encapsulate
  * all control and shepherding of database connections into a single location
  * without the use of globals.
+ *
+ * @see \Drupal\Core\Database\Manager\DatabaseManager
  */
 abstract class Database {
 
@@ -40,47 +44,21 @@ abstract class Database {
   const RETURN_INSERT_ID = 3;
 
   /**
-   * An nested array of all active connections. It is keyed by database name
-   * and target.
-   *
-   * @var array
+   * @var \Drupal\Core\Database\Manager\DatabaseManager
    */
-  static protected $connections = array();
+  protected static $databaseManager;
 
   /**
-   * A processed copy of the database connection information from settings.php.
-   *
-   * @var array
+   * Gets the static instance of self::$databaseManager, and lazy-instantiates
+   * it if needed.
+   * 
+   * @return \Drupal\Core\Database\Manager\DatabaseManager
    */
-  static protected $databaseInfo = array();
-
-  /**
-   * A list of key/target credentials to simply ignore.
-   *
-   * @var array
-   */
-  static protected $ignoreTargets = array();
-
-  /**
-   * The key of the currently active database connection.
-   *
-   * @var string
-   */
-  static protected $activeKey = 'default';
-
-  /**
-   * An array of active query log objects.
-   *
-   * Every connection has one and only one logger object for all targets and
-   * logging keys.
-   *
-   * array(
-   *   '$db_key' => DatabaseLog object.
-   * );
-   *
-   * @var array
-   */
-  static protected $logs = array();
+  public static function getDatabaseManager() {
+    return isset(self::$databaseManager)
+      ? self::$databaseManager
+      : self::$databaseManager = new DatabaseManager();
+  }
 
   /**
    * Starts logging a given logging key on the specified connection.
@@ -98,20 +76,7 @@ abstract class Database {
    * @see \Drupal\Core\Database\Log
    */
   final public static function startLog($logging_key, $key = 'default') {
-    if (empty(self::$logs[$key])) {
-      self::$logs[$key] = new Log($key);
-
-      // Every target already active for this connection key needs to have the
-      // logging object associated with it.
-      if (!empty(self::$connections[$key])) {
-        foreach (self::$connections[$key] as $connection) {
-          $connection->setLogger(self::$logs[$key]);
-        }
-      }
-    }
-
-    self::$logs[$key]->start($logging_key);
-    return self::$logs[$key];
+    return self::getDatabaseManager()->startLog($logging_key, $key);
   }
 
   /**
@@ -133,12 +98,7 @@ abstract class Database {
    * @see \Drupal\Core\Database\Log
    */
   final public static function getLog($logging_key, $key = 'default') {
-    if (empty(self::$logs[$key])) {
-      return NULL;
-    }
-    $queries = self::$logs[$key]->get($logging_key);
-    self::$logs[$key]->end($logging_key);
-    return $queries;
+    return self::getDatabaseManager()->getLog($logging_key, $key);
   }
 
   /**
@@ -153,24 +113,7 @@ abstract class Database {
    *   The corresponding connection object.
    */
   final public static function getConnection($target = 'default', $key = NULL) {
-    if (!isset($key)) {
-      // By default, we want the active connection, set in setActiveConnection.
-      $key = self::$activeKey;
-    }
-    // If the requested target does not exist, or if it is ignored, we fall back
-    // to the default target. The target is typically either "default" or
-    // "replica", indicating to use a replica SQL server if one is available. If
-    // it's not available, then the default/primary server is the correct server
-    // to use.
-    if (!empty(self::$ignoreTargets[$key][$target]) || !isset(self::$databaseInfo[$key][$target])) {
-      $target = 'default';
-    }
-
-    if (!isset(self::$connections[$key][$target])) {
-      // If necessary, a new connection is opened.
-      self::$connections[$key][$target] = self::openConnection($key, $target);
-    }
-    return self::$connections[$key][$target];
+    return self::getDatabaseManager()->getConnection($target, $key);
   }
 
   /**
@@ -184,21 +127,19 @@ abstract class Database {
    *   otherwise.
    */
   final public static function isActiveConnection() {
-    return !empty(self::$activeKey) && !empty(self::$connections) && !empty(self::$connections[self::$activeKey]);
+    return self::getDatabaseManager()->isActiveConnection();
   }
 
   /**
    * Sets the active connection to the specified key.
    *
-   * @return string|null
+   * @param string $key
+   *
+   * @return null|string
    *   The previous database connection key.
    */
   final public static function setActiveConnection($key = 'default') {
-    if (!empty(self::$databaseInfo[$key])) {
-      $old_key = self::$activeKey;
-      self::$activeKey = $key;
-      return $old_key;
-    }
+    return self::getDatabaseManager()->setActiveConnection($key);
   }
 
   /**
@@ -208,28 +149,12 @@ abstract class Database {
    *   The database connection information, as defined in settings.php. The
    *   structure of this array depends on the database driver it is connecting
    *   to.
+   *
+   * @return array
+   *   The modified $info array with database connection information.
    */
   final public static function parseConnectionInfo(array $info) {
-    // If there is no "driver" property, then we assume it's an array of
-    // possible connections for this target. Pick one at random. That allows
-    // us to have, for example, multiple replica servers.
-    if (empty($info['driver'])) {
-      $info = $info[mt_rand(0, count($info) - 1)];
-    }
-    // Parse the prefix information.
-    if (!isset($info['prefix'])) {
-      // Default to an empty prefix.
-      $info['prefix'] = array(
-        'default' => '',
-      );
-    }
-    elseif (!is_array($info['prefix'])) {
-      // Transform the flat form into an array form.
-      $info['prefix'] = array(
-        'default' => $info['prefix'],
-      );
-    }
-    return $info;
+    return self::getDatabaseManager()->parseConnectionInfo($info);
   }
 
   /**
@@ -254,9 +179,7 @@ abstract class Database {
    *   to.
    */
   final public static function addConnectionInfo($key, $target, array $info) {
-    if (empty(self::$databaseInfo[$key][$target])) {
-      self::$databaseInfo[$key][$target] = self::parseConnectionInfo($info);
-    }
+    self::getDatabaseManager()->addConnectionInfo($key, $target, $info);
   }
 
   /**
@@ -268,9 +191,7 @@ abstract class Database {
    * @return array|null
    */
   final public static function getConnectionInfo($key = 'default') {
-    if (!empty(self::$databaseInfo[$key])) {
-      return self::$databaseInfo[$key];
-    }
+    return self::getDatabaseManager()->getConnectionInfo($key);
   }
 
   /**
@@ -279,7 +200,7 @@ abstract class Database {
    * @return array
    */
   final public static function getAllConnectionInfo() {
-    return self::$databaseInfo;
+    return self::getDatabaseManager()->getAllConnectionInfo();
   }
 
   /**
@@ -290,11 +211,7 @@ abstract class Database {
    *   defined in settings.php.
    */
   final public static function setMultipleConnectionInfo(array $databases) {
-    foreach ($databases as $key => $targets) {
-      foreach ($targets as $target => $info) {
-        self::addConnectionInfo($key, $target, $info);
-      }
-    }
+    self::getDatabaseManager()->setMultipleConnectionInfo($databases);
   }
 
   /**
@@ -309,22 +226,7 @@ abstract class Database {
    *   TRUE in case of success, FALSE otherwise.
    */
   final public static function renameConnection($old_key, $new_key) {
-    if (!empty(self::$databaseInfo[$old_key]) && empty(self::$databaseInfo[$new_key])) {
-      // Migrate the database connection information.
-      self::$databaseInfo[$new_key] = self::$databaseInfo[$old_key];
-      unset(self::$databaseInfo[$old_key]);
-
-      // Migrate over the DatabaseConnection object if it exists.
-      if (isset(self::$connections[$old_key])) {
-        self::$connections[$new_key] = self::$connections[$old_key];
-        unset(self::$connections[$old_key]);
-      }
-
-      return TRUE;
-    }
-    else {
-      return FALSE;
-    }
+    return self::getDatabaseManager()->renameConnection($old_key, $new_key);
   }
 
   /**
@@ -337,59 +239,7 @@ abstract class Database {
    *   TRUE in case of success, FALSE otherwise.
    */
   final public static function removeConnection($key) {
-    if (isset(self::$databaseInfo[$key])) {
-      self::closeConnection(NULL, $key);
-      unset(self::$databaseInfo[$key]);
-      return TRUE;
-    }
-    else {
-      return FALSE;
-    }
-  }
-
-  /**
-   * Opens a connection to the server specified by the given key and target.
-   *
-   * @param string $key
-   *   The database connection key, as specified in settings.php. The default is
-   *   "default".
-   * @param string $target
-   *   The database target to open.
-   *
-   * @throws \Drupal\Core\Database\ConnectionNotDefinedException
-   * @throws \Drupal\Core\Database\DriverNotSpecifiedException
-   */
-  final protected static function openConnection($key, $target) {
-    // If the requested database does not exist then it is an unrecoverable
-    // error.
-    if (!isset(self::$databaseInfo[$key])) {
-      throw new ConnectionNotDefinedException('The specified database connection is not defined: ' . $key);
-    }
-
-    if (!$driver = self::$databaseInfo[$key][$target]['driver']) {
-      throw new DriverNotSpecifiedException('Driver not specified for this database connection: ' . $key);
-    }
-
-    if (!empty(self::$databaseInfo[$key][$target]['namespace'])) {
-      $driver_class = self::$databaseInfo[$key][$target]['namespace'] . '\\Connection';
-    }
-    else {
-      // Fallback for Drupal 7 settings.php.
-      $driver_class = "Drupal\\Core\\Database\\Driver\\{$driver}\\Connection";
-    }
-
-    $pdo_connection = $driver_class::open(self::$databaseInfo[$key][$target]);
-    $new_connection = new $driver_class($pdo_connection, self::$databaseInfo[$key][$target]);
-    $new_connection->setTarget($target);
-    $new_connection->setKey($key);
-
-    // If we have any active logging objects for this connection key, we need
-    // to associate them with the connection we just opened.
-    if (!empty(self::$logs[$key])) {
-      $new_connection->setLogger(self::$logs[$key]);
-    }
-
-    return $new_connection;
+    return self::getDatabaseManager()->removeConnection($key);
   }
 
   /**
@@ -402,30 +252,7 @@ abstract class Database {
    *   The database connection key. Defaults to NULL which means the active key.
    */
   public static function closeConnection($target = NULL, $key = NULL) {
-    // Gets the active connection by default.
-    if (!isset($key)) {
-      $key = self::$activeKey;
-    }
-    // To close a connection, it needs to be set to NULL and removed from the
-    // static variable. In all cases, closeConnection() might be called for a
-    // connection that was not opened yet, in which case the key is not defined
-    // yet and we just ensure that the connection key is undefined.
-    if (isset($target)) {
-      if (isset(self::$connections[$key][$target])) {
-        self::$connections[$key][$target]->destroy();
-        self::$connections[$key][$target] = NULL;
-      }
-      unset(self::$connections[$key][$target]);
-    }
-    else {
-      if (isset(self::$connections[$key])) {
-        foreach (self::$connections[$key] as $target => $connection) {
-          self::$connections[$key][$target]->destroy();
-          self::$connections[$key][$target] = NULL;
-        }
-      }
-      unset(self::$connections[$key]);
-    }
+    self::getDatabaseManager()->closeConnection($target, $key);
   }
 
   /**
@@ -441,6 +268,6 @@ abstract class Database {
    *   The target of the specified key to ignore.
    */
   public static function ignoreTarget($key, $target) {
-    self::$ignoreTargets[$key][$target] = TRUE;
+    self::getDatabaseManager()->ignoreTarget($key, $target);
   }
 }
