a1c9c769e1729965746004411e54dcf4da99f233
Andreas Hennings <andreas@dqxtech.net>
2014-10-27 06:05:21 +0100

    tmp2 SiteSettings

--- core/lib/Drupal/Core/Site/SiteSettings.php
+++ core/lib/Drupal/Core/Site/SiteSettings.php
@@ -3,70 +3,94 @@
 
 namespace Drupal\Core\Site;
 
+use Drupal\Component\Utility\String;
+use Drupal\Core\Site\Settings\SettingsInstance;
+
 /**
  * @property string|null baseUrl
+ *   The $base_url value from settings.php
  * @property string|null cookieDomain
+ *   The $cookie_domain value from settings.php
  * @property string[]|null configDirectories
- * @property array settings
+ *   The $config_directories array from settings.php
  * @property array config
+ *   The $config array from settings.php
  * @property array[][] databases
+ *   The $databases array from settings.php.
+ * @property \Drupal\Core\Site\Settings\SettingsInterface settings
+ *   Object encapsulating the $settings array from settings.php.
  */
-class SiteSettings extends MiniContainer {
+class SiteSettings {
 
   /**
    * Array with the settings.
    *
    * @var mixed[]
    */
-  private $storage = array();
+  private $values;
+
+  /**
+   * @var \Composer\Autoload\ClassLoader|null
+   */
+  private $classLoader;
 
   /**
    * Constructor.
    *
-   * @param array $settings
-   *   Array with the settings.
+   * @param mixed[] $values
+   *   Array with the variables from settings.php.
    */
-  public function __construct(array $settings) {
-    $this->storage = $settings;
+  public function __construct(array $values) {
+
+    $values += array(
+      'base_url' => NULL,
+      'cookie_domain' => NULL,
+      'config_directories' => array(),
+      'config' => array(),
+      'databases' => array(),
+      'settings' => array(),
+      'class_loader' => NULL,
+    );
+
+    // @todo Validate all values, and throw \InvalidArgumentException if invalid.
+
+    $this->values = array(
+      'baseUrl' => $values['base_url'],
+      'cookieDomain' => $values['cookie_domain'],
+      'configDirectories' => $values['config_directories'],
+      'config' => $values['config'],
+      'databases' => $values['databases'],
+      'settings' => new SettingsInstance($values['settings']),
+    );
+
+    $this->classLoader = $values['class_loader'];
   }
 
   /**
-   * @param string $name
-   * @param mixed $default
+   * Magic __get() method. This allows to take advantage of "@property" in the
+   * class docblock.
+   *
+   * @param string $key
    *
    * @return mixed
+   * @throws \InvalidArgumentException
    */
-  public function get($name, $default = NULL) {
-    return isset($this->storage[$name])
-      ? $this->storage[$name]
-      : $default;
-  }
-
-  /**
-   * @return mixed[]
-   */
-  public function getAll() {
-    return $this->storage;
-  }
-
-  /**
-   * Gets a salt useful for hardening against SQL injection.
-   *
-   * @return string
-   *   A salt based on information in settings.php, not in the database.
-   *
-   * @throws \RuntimeException
-   */
-  public function getHashSalt() {
-    $hash_salt = $this->get('hash_salt');
-    // This should never happen, as it breaks user logins and many other
-    // services. Therefore, explicitly notify the user (developer) by throwing
-    // an exception.
-    if (empty($hash_salt)) {
-      throw new \RuntimeException('Missing $settings[\'hash_salt\'] in settings.php.');
+  public function __get($key) {
+    if (!array_key_exists($key, $this->values)) {
+      throw new \InvalidArgumentException(
+        String::format("Settings key '@key' does not exist.", array('@key' => $key)));
     }
+    return $this->values[$key];
+  }
 
-    return $hash_salt;
+  /**
+   * Exports some of the settings to global values.
+   */
+  public function exportGlobals() {
+    $GLOBALS['base_url'] = $this->values['baseUrl'];
+    $GLOBALS['cookie_domain'] = $this->values['cookieDomain'];
+    $GLOBALS['config_directories'] = $this->values['configDirectories'];
+    $GLOBALS['config'] = $this->values['config'];
   }
 
 }
