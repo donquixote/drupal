0833fe9752851b3b2244f6e151e153be26fff3c1
Andreas Hennings <andreas@dqxtech.net>
2016-05-11 02:06:37 +0200

    Create UtilBase class, as a base class for utility classes.

---
+++ core/lib/Drupal/Component/Utility/UtilBase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\Component\Utility;
+
+/**
+ * Base class for "utility" classes, which typically only contain static
+ * methods.
+ */
+abstract class UtilBase {
+
+  /**
+   * Private constructor.
+   */
+  private function __construct() {}
+
+}
