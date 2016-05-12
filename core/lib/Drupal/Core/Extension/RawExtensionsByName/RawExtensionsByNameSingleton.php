0d2e74fdd33fbfc14634273737712390b8338d36
Andreas Hennings <andreas@dqxtech.net>
2016-05-12 02:48:04 +0200

    New extension system: RawExtensionsByNameSingleton now uses RawExtensionsByType*.

--- core/lib/Drupal/Core/Extension/RawExtensionsByName/RawExtensionsByNameSingleton.php
+++ core/lib/Drupal/Core/Extension/RawExtensionsByName/RawExtensionsByNameSingleton.php
@@ -3,9 +3,7 @@
 namespace Drupal\Core\Extension\RawExtensionsByName;
 
 use Drupal\Component\Utility\UtilBase;
-use Drupal\Core\Extension\ProfileDirs\ProfileDirsSingleton;
-use Drupal\Core\Extension\SearchdirPrefixes\SearchdirPrefixes_Common;
-use Drupal\Core\Extension\SearchdirToRawExtensionsGrouped\SearchdirToRawExtensionsGroupedSingleton;
+use Drupal\Core\Extension\RawExtensionsByType\RawExtensionsByTypeSingleton;
 
 final class RawExtensionsByNameSingleton extends UtilBase {
 
@@ -25,26 +23,9 @@ final class RawExtensionsByNameSingleton extends UtilBase {
   public static function getInstance($root, $type, $include_tests = FALSE) {
     return isset(self::$instances[$root][$type][(int)$include_tests])
       ? self::$instances[$root][$type][(int)$include_tests]
-      : self::$instances[$root][$type][(int)$include_tests] = self::createInstanceFromSingletons($root, $type, $include_tests);
-  }
-
-  /**
-   * @param string $root
-   * @param string $type
-   * @param bool $include_tests
-   *
-   * @return \Drupal\Core\Extension\RawExtensionsByName\RawExtensionsByNameInterface
-   */
-  private static function createInstanceFromSingletons($root, $type, $include_tests) {
-    $searchdirPrefixes = new SearchdirPrefixes_Common();
-    $searchdirToRawExtensionsGrouped = SearchdirToRawExtensionsGroupedSingleton::getInstance($root, $include_tests);
-    if ($type === 'profile') {
-      return new RawExtensionsByName_Profile($searchdirPrefixes, $searchdirToRawExtensionsGrouped, 'profile');
-    }
-    else {
-      $activeProfileDirs = ProfileDirsSingleton::getInstance($root, $include_tests);
-      return new RawExtensionsByName_NonProfile($searchdirPrefixes, $searchdirToRawExtensionsGrouped, $type, $activeProfileDirs);
-    }
+      : self::$instances[$root][$type][(int)$include_tests] = new RawExtensionsByName_FromRawExtensionsByType(
+        RawExtensionsByTypeSingleton::getInstance($root, $include_tests),
+        $type);
   }
 
 }
